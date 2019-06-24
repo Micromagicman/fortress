012de08bcbe3414116ab24246e855f818b758aec
Evgeniy <evgen@micromagicman.ru>
2019-06-24 20:03:35 +0300

    database transactions + new exception info

--- framework/core/exception/DatabaseQueryException.php
+++ framework/core/exception/DatabaseQueryException.php
@@ -2,13 +2,22 @@
 
 namespace fortress\core\exception;
 
+use PDOStatement;
+
 class DatabaseQueryException extends FortressException {
 
     private $query;
 
-    public function __construct(string $message, string $query) {
-        parent::__construct("$message on: '$query'");
-        $this->query = $query;
+    public function __construct(string $message, PDOStatement $pdoStatement) {
+        parent::__construct(
+            sprintf(
+                "%s on: '%s'. PDO message: %s",
+                $message,
+                $pdoStatement->queryString,
+                $pdoStatement->errorInfo()[2]
+            ),
+            $pdoStatement->errorCode());
+        $this->query = $pdoStatement->queryString;
     }
 
     public function getQuery() {
