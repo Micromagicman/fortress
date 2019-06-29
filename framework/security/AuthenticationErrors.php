1a7bf095b33713e287b61d5aec79fede4317db8f
Evgeniy <evgen@micromagicman.ru>
2019-06-29 10:15:38 +0300

    AuthenticationErrors class

--- framework/security/AuthenticationErrors.php
+++ framework/security/AuthenticationErrors.php
@@ -1,9 +1,24 @@
 <?php
 
-
 namespace fortress\security;
-
 
 class AuthenticationErrors {
 
+    private const ERRORS_SESSION_KEY = "AUTHENTICATION_ERRORS";
+
+    private $session;
+
+    public function __construct(Session $session) {
+        $this->session = $session;
+    }
+
+    public function setErrors(string ...$errors) {
+        $this->session->set(self::ERRORS_SESSION_KEY, $errors);
+    }
+
+    public function getLastErrors() {
+        $errors = $this->session->get(self::ERRORS_SESSION_KEY);
+        $this->session->delete(self::ERRORS_SESSION_KEY);
+        return $errors;
+    }
 }
--- framework/security/basic/BaseAuthenticator.php
+++ framework/security/basic/BaseAuthenticator.php
@@ -2,37 +2,46 @@
 
 namespace fortress\security\basic;
 
+use fortress\security\AuthenticationErrors;
 use fortress\security\Authenticator;
 use fortress\security\Session;
 use fortress\security\UserProvider;
 
 class BaseAuthenticator implements Authenticator {
 
+    private const USER_SESSION_KEY = "AUTHORIZED_USER";
+
     private $session;
-
     private $userProvider;
+    private $authenticationErrors;
 
-    public function __construct(Session $session, UserProvider $userProvider) {
+    public function __construct(
+        Session $session,
+        UserProvider $userProvider,
+        AuthenticationErrors $errors
+    ) {
         $this->session = $session;
+        $this->authenticationErrors = $errors;
         $this->userProvider = $userProvider;
     }
 
     public function authenticate(string $username, string $password) {
         $user = $this->userProvider->byUsername($username);
         if (password_verify($password, $user->getPassword())) {
-            $this->session->set("AUTHORIZED_USER", $user->serialize());
+            $this->session->set(self::USER_SESSION_KEY, $user->serialize());
             return true;
         }
+        $this->authenticationErrors->setErrors("Неверное имя пользователя или пароль");
         return false;
     }
 
     public function logout() {
-        $this->session->delete("AUTHORIZED_USER");
+        $this->session->delete(self::USER_SESSION_KEY);
         return true;
     }
 
     public function loadUser() {
-        $userData = $this->session->get("AUTHORIZED_USER");
+        $userData = $this->session->get(self::USER_SESSION_KEY);
         $user = new BaseUser();
         if (null != $userData) {
             $user->unserialize($userData);
