f79d6f987a4e1606590dcc95c37938c421aa0ffc
micromagicman <evgen@micromagicman.ru>
2020-01-25 11:11:58 +0300

    CI+

--- framework/command/CreateAppCommand.php
+++ framework/command/CreateAppCommand.php
@@ -92,13 +92,4 @@ class CreateAppCommand extends Command {
         }
         closedir($sourceDirResource);
     }
-
-    /**
-     * Вывод строки с текущим временем в stdOut
-     * @param string $line
-     */
-    private function writeWithData(string $line) {
-        echo "[" . date("Y-m-d, H:i") . "] " . $line . "\n";
-    }
-
 }
